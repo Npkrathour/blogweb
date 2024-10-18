7efe0e78d3e33c5947605d8ece15415037484f8c
Pinku rathour <pinkurathour2016@gmail.com>
2024-10-18 14:34:31 +0530

    add map function

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,7 +1,5 @@
 import post from "../../assets/post_1.jpg";
-import post7 from "../../assets/post_7.jpg";
-import post8 from "../../assets/post_8.jpg";
-import post9 from "../../assets/post_9.jpg";
+import RecentPost from "../Post/RecentPost";
 const Home = () => {
   return (
     <>
@@ -23,42 +21,7 @@ const Home = () => {
               </a>
             </div>
             <div className="recentPost">
-              <div className="flex justify-between items-center gap-2 py-4">
-                <div className="w-full max-w-36">
-                  <img src={post7} alt="" className=" w-full h-full object-cover" />
-                </div>
-                <div>
-                  <span className="bg-red-600 text-white px-4 font-inter text-base py-1">Food</span>
-                  <h3 className="text-lg font-medium font-inter py-4">
-                    Crispy Air Fryer Parmesan And Thyme Roasted Wedge Fries
-                  </h3>
-                  <h4 className="text-base font-normal font-inter">ByXu Jianhong</h4>
-                </div>
-              </div>
-              <div className="flex justify-between items-center gap-2 py-4">
-                <div className="w-full max-w-36">
-                  <img src={post8} alt="" className=" w-full h-full object-cover" />
-                </div>
-                <div>
-                  <span className="bg-red-600 text-white px-4 font-inter text-base py-1">Food</span>
-                  <h3 className="text-lg font-medium font-inter py-4">
-                    Crispy Air Fryer Parmesan And Thyme Roasted Wedge Fries
-                  </h3>
-                  <h4 className="text-base font-normal font-inter">ByXu Jianhong</h4>
-                </div>
-              </div>
-              <div className="flex justify-between items-center gap-2 py-4">
-                <div className="w-full max-w-36">
-                  <img src={post9} alt="" className=" w-full h-full object-cover" />
-                </div>
-                <div>
-                  <span className="bg-red-600 text-white px-4 font-inter text-base py-1">Food</span>
-                  <h3 className="text-lg font-medium font-inter py-4">
-                    Crispy Air Fryer Parmesan And Thyme Roasted Wedge Fries
-                  </h3>
-                  <h4 className="text-base font-normal font-inter">ByXu Jianhong</h4>
-                </div>
-              </div>
+              <RecentPost />
             </div>
           </div>
         </div>
--- src/Components/Post/RecentPost.jsx
+++ src/Components/Post/RecentPost.jsx
@@ -1,16 +1,46 @@
-const RecentPost = ({ id, postTitle, postImage, postName }) => {
+import post7 from "../../assets/post_7.jpg";
+import post8 from "../../assets/post_8.jpg";
+import post9 from "../../assets/post_9.jpg";
+
+const RecentPost = () => {
+  const userData = [
+    {
+      postTitle: "Crispy Air Fryer Parmesan And Thyme Roasted Wedge Fries",
+      postName: "Chef John's Recipe",
+      postImage: post7,
+      bgColor: "bg-red-600",
+      tagName: "Food",
+    },
+    {
+      postTitle: "Get Around Easily With A New York Limousine Service",
+      postName: "Healthy Meals",
+      postImage: post8,
+      bgColor: "bg-blue-600",
+      tagName: "Nature",
+    },
+    {
+      postTitle: "Kipchoge Proves He Has No Equal Runs 2nd Fastest Marathon Time In History",
+      postName: "Best Eats",
+      postImage: post9,
+      bgColor: "bg-green-600",
+      tagName: "cycle",
+    },
+  ];
+
   return (
     <>
-      <div className="flex justify-between items-center gap-2 py-4">
-        <div className="w-full max-w-36">
-          <img src={postImage} alt="" className=" w-full h-full object-cover" />
+      {userData.map(({ postTitle, postName, postImage, bgColor, tagName }, index) => (
+        <div key={index} className="flex items-center gap-2 py-4">
+          <div className="w-full max-w-36">
+            <img src={postImage} alt={postTitle} className="w-full h-full object-cover" />
+          </div>
+          <div>
+            <span className={`${bgColor} text-white px-4 font-inter text-base py-1`}>{tagName}</span>
+            <h3 className="text-lg font-medium font-inter py-4">{postTitle}</h3>
+            <h4 className="text-base font-normal font-inter">{postName}</h4>
+          </div>
         </div>
-        <div>
-          <span className="bg-red-600 text-white px-4 font-inter text-base py-1">Food</span>
-          <h3 className="text-lg font-medium font-inter py-4">{title}</h3>
-          <h4 className="text-base font-normal font-inter">{postName}</h4>
-        </div>
-      </div>
+      ))}
     </>
   );
 };
